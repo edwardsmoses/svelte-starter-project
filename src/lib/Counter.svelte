95eee855eeef002a437e91fd9356fe79a4cbf7c5
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-25 17:13:49 +0100

    understanding event binds

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
   let count: number = 2;
+  let name: string = "";
   const increment = () => {
     count = count * count;
-  }
+  };
 </script>
 
 <button on:click={increment}>
   count is {count}
 </button>
+<h1>My name is {name}</h1>
+<input
+  value={name}
+  on:input={(event) => {
+    name = event.currentTarget.value;
+  }}
+/>
